54ea66720143c7b6665936b32052f1a84e20822c
Anna Hill <annahill@Annas-MacBook-Air.local>
2024-09-06 21:48:53 -0500

    v1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//variables
+int numberOfSandwiches = 0;
+int numberOfToppings = 0;
+double tip = 0;
+
+const double COST_OF_SANDWICH = 4.75;
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.10;
+
+System.Console.WriteLine("How many sandwiches?");
+numberOfSandwiches = int.Parse(Console.ReadLine());
