b2264121e2822c7e824f7639a5d364532d65b47f
Anna Hill <annahill@Annas-MacBook-Air.local>
2024-09-09 10:00:36 -0500

    v1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿//variables
+
 int numberOfSandwiches = 0;
 int numberOfToppings = 0;
 double tip = 0;
@@ -9,3 +10,11 @@ const double DISCOUNT_AMOUNT = 0.10;
 
 System.Console.WriteLine("How many sandwiches?");
 numberOfSandwiches = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("How many toppings?");
+numberOfToppings = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("How much would you like to tip?");
+tip = double.Parse(Console.ReadLine());
+
+double totalSandwichCost = COST_OF_SANDWICH * numberOfSandwiches;
